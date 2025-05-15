b75bb65ed1ccd931c1364d095267308889df96ca
Khang Chinh <luckydoom000@gmail.com>
2025-05-15 19:12:07 +0700

    fe

--- src/containers/OwnerPage/Owner.js
+++ src/containers/OwnerPage/Owner.js
@@ -73,7 +73,7 @@ class Owner extends Component {
     await this.handleLoadProductTypeFilterValue();
     await this.handleLoadPetTypeFilterValue();
   }
-  async componentDidUpdate(prevState) {
+  async componentDidUpdate(prevProps, prevState) {
     if (prevState.actionPage !== this.state.actionPage) {
       const { actionPage, loadedProductTypeFilterValue, loadedPetTypeFilterValue, loadedPaymentStatusFilterValue, loadedShippingStatusFilterValue, loadedBannerStatusFilterValue } = this.state;
       switch (this.state.actionPage) {
@@ -614,6 +614,7 @@ class Owner extends Component {
   handleChangeProductFromModal = async (productInfo) => {
     this.setState({ isLoading: true });
     try {
+      console.log(productInfo)
       const response = await handleChangeProductInfoApi(productInfo);
       if (response && response.errCode === 0) {
         toast.success('Chỉnh sửa thông tin sản phẩm thành công!', {
@@ -1442,19 +1443,19 @@ class Owner extends Component {
                           <td>
                             {item.CreatedAt
                               ? new Date(item.CreatedAt).toLocaleString('vi-VN', {
-                                  day: '2-digit',
-                                  month: '2-digit',
-                                  year: 'numeric',
-                                })
+                                day: '2-digit',
+                                month: '2-digit',
+                                year: 'numeric',
+                              })
                               : 'N/A'}
                           </td>
                           <td>
                             {item.HiddenAt
                               ? new Date(item.HiddenAt).toLocaleString('vi-VN', {
-                                  day: '2-digit',
-                                  month: '2-digit',
-                                  year: 'numeric',
-                                })
+                                day: '2-digit',
+                                month: '2-digit',
+                                year: 'numeric',
+                              })
                               : 'Vô thời hạn'}
                           </td>
                           <td className="f" onClick={(e) => e.stopPropagation()}>
--- src/containers/OwnerPage/ViewInvoiceModal.js
+++ src/containers/OwnerPage/ViewInvoiceModal.js
@@ -23,9 +23,6 @@ class ViewInvoiceModal extends Component {
     if (this.props.selectedInvoiceID) {
       await this.handleLoadInvoiceDetails(this.props.selectedInvoiceID);
     }
-    setTimeout(() => {
-      console.log(this.state.loadedInvoiceDetails);
-    });
   }
 
   async componentDidUpdate(prevProps) {
@@ -163,13 +160,13 @@ class ViewInvoiceModal extends Component {
                         Thời gian:{' '}
                         {loadedInvoiceDetails.CreatedAt
                           ? new Date(loadedInvoiceDetails.CreatedAt).toLocaleString('vi-VN', {
-                              day: '2-digit',
-                              month: '2-digit',
-                              year: 'numeric',
-                              hour: '2-digit',
-                              minute: '2-digit',
-                              second: '2-digit',
-                            })
+                            day: '2-digit',
+                            month: '2-digit',
+                            year: 'numeric',
+                            hour: '2-digit',
+                            minute: '2-digit',
+                            second: '2-digit',
+                          })
                           : 'N/A'}
                       </p>
                     </div>
