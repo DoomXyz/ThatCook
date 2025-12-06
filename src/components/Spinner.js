9f792d8a47b9163662311c852a3be5135124544c
Mincow <pkminh271104@gmail.com>
2025-12-06 19:55:22 +0700

    bce

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import { createPortal } from 'react-dom';
 import './Spinner.scss';
 
 const Spinner = () => {
   const circleElements = Array.from({ length: 12 }, (_, index) => <div key={index} className={`sk-circle${index + 1} sk-child`}></div>);
 
-  return (
+  return createPortal(
     <div className="sk-circle-container">
       <div className="sk-circle">{circleElements}</div>
       <p className="loading-text">Loading...</p>
-    </div>
+    </div>,
+    document.body
   );
 };
 
